04597dbefd8aa0a448486c35e647d03a22a0606f
Mokaddes-Ali <mokaddes.ru2000@gmail.com>
2025-09-21 22:37:48 +0600

    add code commit

--- assets/main.js
+++ assets/main.js
@@ -5,4 +5,19 @@ jQuery(document).ready( function(){
           $(".product-cart-popup").addClass("hidden");
     });
 
+
+
+    $(".product_quick_view").on("click", function(){
+        $(".product-cart-popup").removeClass("hidden");
+
+        $.ajax({
+      type: "GET",
+      url: "/cart.js",
+      dataType: "json",
+      success: function (cart) {
+        updateCartUI(cart);
+      }
+    });
+    });
+
 });
