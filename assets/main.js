150c04cf6be08fc02552a3ae15343863e1e3e0bd
Mokaddes-Ali <mokaddes.ru2000@gmail.com>
2025-09-24 07:03:26 +0600

    data show in wishlist commit

--- assets/main.js
+++ assets/main.js
@@ -23,8 +23,9 @@ jQuery(document).ready( function(){
 
     // close_cart_drapper
     $('.close_cart_drapper').on("click", function(){
-         $(".cart-popup").addClass("hidden");
-          $(".product-cart-popup").addClass("hidden");
+        $(".cart-popup").addClass("hidden");
+        $(".product-cart-popup").addClass("hidden");
+        $(".wishlist-cart-popup").addClass("hidden");
     });
 
       $(".product_quick_view").on("click", function(){
@@ -146,5 +147,129 @@ for (let i = 1; i <= 5; i++) {
     }
   });
 });
+
+
+      // Add to wishlist
+            $(".add_wishlist").on("click", function () {
+            var card = $(this).closest(".wishlist-card");
+            $(this).addClass("hidden");
+            card.find(".close_wishlist").removeClass("hidden");
+
+            let product_id = $(this).data("product_id");
+
+            let wishList = JSON.parse(localStorage.getItem("wishlist") || "[]");
+            if (!wishList.includes(product_id)) {
+                wishList.push(product_id);
+                localStorage.setItem("wishlist", JSON.stringify(wishList));
+                $(".show-wishlist-btn span").text(wishList.length);
+
+            }
+            });
+
+            // Remove from wishlist
+            $(".close_wishlist").on("click", function () {
+            var card = $(this).closest(".wishlist-card");
+            $(this).addClass("hidden");
+            card.find(".add_wishlist").removeClass("hidden");
+
+            let product_id = $(this).data("product_id");
+
+            let wishList = JSON.parse(localStorage.getItem("wishlist") || "[]");
+            wishList = wishList.filter((id) => id !== product_id);
+            localStorage.setItem("wishlist", JSON.stringify(wishList));
+            $(".show-wishlist-btn span").text(wishList.length);
+            });
+        //   loading stay wishlist button
+            $(".wishlist-card").each(function () {
+            let product_id = $(this)
+                .find(".add_wishlist, .close_wishlist")
+                .data("product_id");
+
+            let wishList = JSON.parse(localStorage.getItem("wishlist") || "[]");
+            if (wishList.includes(product_id)) {
+                $(this).find(".add_wishlist").addClass("hidden");
+                $(this).find(".close_wishlist").removeClass("hidden");
+                $(".show-wishlist-btn span").text(wishList.length);
+            }
+            });
+
+           $(".show-wishlist-btn").on("click", function(){
+               let wishList_card = localStorage.getItem("wishlist");
+               if(wishList_card){
+                 $(".wishlist-cart-popup").removeClass("hidden");
+                 wishList_card = JSON.parse(wishList_card);
+
+                 $(".wishlist-drawer-items").empty();
+                 
+                 wishList_card.forEach( function(product_handle){
+                  console.log(product_handle);
+                   $.ajax({
+                        type: "GET",
+                        url: `/products/${product_handle}.js`,
+                        dataType: "json",
+                        success: function (product) {
+                        console.log(product);
+
+          $(".wishlist-drawer-items").append(`
+         <section class="product-template flex gap-4 items-center">
+                  <!-- image  -->
+              <div class="w-4/12">
+                  <div class="w-[144px] h-[130px]">
+                    <img src="${ product.featured_image }" alt="${ product.title }" class="w-full h-full object-contain"/>
+                  </div>
+              </div>
+
+    
+    <div class="product-details-right w-8/12 flex flex-col gap-3 py-1.5">
+
+        <!-- product text -->
+        <div class="text-area-wrapper w-full flex flex-col gap-[15.30px]">
+            <h1 class="text-black font-[500] max-w-lg text-base not-italic leading-none font-integralcf">
+               ${ product.title }
+                </h1>
+
+            <!-- product price -->
+            <div class="product-price py-1">
+                <h1 class="text-black font-satoshi text-base not-italic font-extrabold leading-none">
+                $${ (product.price /100).toFixed(2) }
+                </h1>  
+            </div>
+          </div>
+
+    <div class="cart-btn flex gap-3">
+       <form action="/cart" method="post" class="flex items-center gap-4">
+  <div class="flex px-5 py-3 gap-3 items-center flex-shrink-0 rounded-[62px] bg-[#F0F0F0]">
+      <!-- Minus Icon -->
+      <svg class="minus-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
+        <path d="M21.375 12C21.375 12.2984 21.2565 12.5845 21.0455 12.7955C20.8345 13.0065 20.5484 13.125 20.25 13.125H3.75C3.45163 13.125 3.16548 13.0065 2.9545 12.7955C2.74353 12.5845 2.625 12.2984 2.625 12C2.625 11.7016 2.74353 11.4155 2.9545 11.2045C3.16548 10.9935 3.45163 10.875 3.75 10.875H20.25C20.5484 10.875 20.8345 10.9935 21.0455 11.2045C21.2565 11.4155 21.375 11.7016 21.375 12Z" fill="black"/>
+      </svg>
+
+      <span class="product-quantity text-black font-satoshi text-[16px] text-center">1</span>
+
+      <input type="hidden" name="quantity" value="1" min="1" />
+
+      <!-- Plus Icon -->
+      <svg class="plus-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+        <path d="M21.375 12C21.375 12.2984 21.2565 12.5845 21.0455 12.7955C20.8345 13.0065 20.5484 13.125 20.25 13.125H13.125V20.25C13.125 20.5484 13.0065 20.8345 12.7955 21.0455C12.5845 21.2565 12.2984 21.375 12 21.375C11.7016 21.375 11.4155 21.2565 11.2045 21.0455C10.9935 20.8345 10.875 20.5484 10.875 20.25V13.125H3.75C3.45163 13.125 3.16548 13.0065 2.9545 12.7955C2.74353 12.5845 2.625 12.2984 2.625 12C2.625 11.7016 2.74353 11.4155 2.9545 11.2045C3.16548 10.9935 3.45163 10.875 3.75 10.875H10.875V3.75C10.875 3.45163 10.9935 3.16548 11.2045 2.9545C11.4155 2.74353 11.7016 2.625 12 2.625C12.2984 2.625 12.5845 2.74353 12.7955 2.9545C13.0065 3.16548 13.125 3.45163 13.125 3.75V10.875H20.25C20.5484 10.875 20.8345 10.9935 21.0455 11.2045C21.2565 11.4155 21.375 11.7016 21.375 12Z" fill="black"/>
+      </svg>
+  </div>
+</form>
+
+      <button id="add-to-cart"
+        class="disabled:cursor-not-allowed disabled:opacity-50 flex px-6 py-3 justify-center items-center gap-[12px] flex-shrink-0 rounded-[62px] bg-black text-white font-[Satoshi] text-sm not-italic font-medium leading-normal"
+        type="submit">
+        Add to Cart
+      </button>
+    </div>
+</section>`
+          )
+        }
+          });
+                 })
+
+               }
+
+             });
+            
 
 });
